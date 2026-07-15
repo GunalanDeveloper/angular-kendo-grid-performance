df3ec499ceb41b0760db5b2fe18e7ee34de6a383
Gunalan <114910887+GunalanDeveloper@users.noreply.github.com>
2026-07-15 14:16:03 +0530

    Update app config with FormsModule support for ngModel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideAnimations(), provideHttpClient()]
